9152beb72e867f20f200e728b08e282bd5b6b253
Lawremwashail <lawremwash@gmail.com>
2024-06-18 20:44:08 +0000

    subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,7 +15,16 @@ def number_of_subscribers(subreddit):
             "User-Agent": "Custom User-Agent (by /u/yourself)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+
+    if response.status_code == 200:
+        results = response.json()
+
+        if 'data' in results and 'subscribers' in results['data']:
+            return results['data']['subscribers']
+
+        else:
+            return 0
+    elif response.status_code == 404:
         return 0
-    results = response.json().get('data')
-    return results.get('subscribers')
+    else:
+        return 0
