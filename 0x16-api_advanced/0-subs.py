c3c1f247c572073a18c2d7b1c70e8225d9d9aff9
Lawremwashail <lawremwash@gmail.com>
2024-06-18 21:40:04 +0000

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,6 +10,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """Returns total number of subscribers in a subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
             "User-Agent": "Custom User-Agent (by /u/yourself)"
