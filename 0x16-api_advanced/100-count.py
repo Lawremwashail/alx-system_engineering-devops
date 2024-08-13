7c72c4542f2bc489ce5679237986d383c30dbbae
Lawremwashail <lawremwash@gmail.com>
2024-08-13 03:43:18 -0700

    100-count.py

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API, parses the title of all hot articles,
+and prints a sorted count of given keywords
+"""
+
+
+import requests
+
+
+
+def count_words(subreddit, words, instances={}, after="", count=0):
+    headers = {"User-Agent": "reddit:top_ten:v1.0 (by /u/yourusername)"}
+    params = {
+            'after': after,
+            'count': count,
+            'limit': 100
+    }
+    response = requests.get(
+            "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+            headers=headers, params=params, allow_redirects=False
+    )
+    try:
+        results = response.json()
+        if response.status_code == 404:
+            raise Exception
+        except Exception:
+            print("")
+            return
+
+    results = results.get("data")
+    after = results.get("after")
+    count = results.get("dist")
+
+    for child in results.get("children"):
+        title = child.get("data").get("title").lower().split()
+        for word in words:
+            if word.lower() in title:
+                times = title.count(word.lower())
+                if instances.get(word.lower()) is None:
+                    instances[word.lower()] = times
+                else:
+                    instances[word.lower()] += times
+
+        if after is None:
+            if len(instances) == 0:
+                print("")
+                return
+
+            instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+            [print("{}: {}".format(k, v)) for k, v in instances]
+        else:
+            count_words(subreddit, words, instances, after, count)
