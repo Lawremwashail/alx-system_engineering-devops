fb7175e334d2a09af3b4455a6d09df70abdf15ba
Lawremwashail <lawremwash@gmail.com>
2024-08-13 02:42:52 -0700

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 """
-function that queries the Reddit API, parses the title of all hot articles
-and prints a sorted count of given keywords
+Function that queries the Reddit API and returns a list containing
+the titles of all hot articles for a given subreddit
 """
-
 
 import requests
 
@@ -20,7 +19,7 @@ def recurse(subreddit, hot_list=[], after="", count=0):
             "https://www.reddit.com/r/{}/hot.json".format(subreddit),
             headers=headers, params=params, allow_redirects=False
     )
-    if response.status_code != 200:
+    if response.status_code == 404:
         return None
 
     results = response.json().get("data")
