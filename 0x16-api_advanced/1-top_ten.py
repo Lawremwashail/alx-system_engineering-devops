14bd4a21ef5b48fc5fe34ad7a875eb48716b9ea2
Lawremwashail <lawremwash@gmail.com>
2024-08-15 13:23:44 -0700

    topten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,6 +7,18 @@ Function that queries and prints the titles of the first 10 hot posts
 
 def top_ten(subreddit):
 
+    """
+    Function that queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit.
+
+    Args:
+        subreddit (str): The subreddit to search.
+
+    Returns:
+        None: Prints the titles of the first 10 hot
+        posts or None if the subreddit is invalid.
+    """
+
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "reddit:top_ten:v1.0 (by /u/yourusername)"}
     params = {"limit": 10}
@@ -14,11 +26,11 @@ def top_ten(subreddit):
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
     if response.status_code == 200:
-        data = response.json().get('data')
+        data = response.json().get('data', {})
 
     children = data.get("children", [])
 
-    for child in children[:10]:
+    for child in children:
         title = child.get("data", {}).get("title")
         if title:
             print(title)
