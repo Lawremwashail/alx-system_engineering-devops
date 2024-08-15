dd4ebe6a0b30cef75517c66f4b9810017b9d44ea
Lawremwashail <lawremwash@gmail.com>
2024-08-15 13:16:29 -0700

    recurssw

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,6 +9,25 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
+
+    """
+    Recursive function that queries the Reddit API and returns a
+    list containing
+    the titles of all hot articles for a given subreddit.
+
+    Args:
+        subreddit (str): The subreddit to search.
+        hot_list (list, optional): A list to store the titles o
+        hot articlest.
+        after (str, optional): A pagination parameter for Reddit API.
+
+    Returns:
+        list or None: A list of titles of hot articles for the
+        given subreddit. 
+                      Returns None if the subreddit is invalid or
+                      no results are found.
+    """
+
     headers = {"User-Agent": "reddit:top_ten:v1.0 (by /u/yourusername)"}
     params = {
             'after': after,
@@ -19,16 +38,20 @@ def recurse(subreddit, hot_list=[], after="", count=0):
             "https://www.reddit.com/r/{}/hot.json".format(subreddit),
             headers=headers, params=params, allow_redirects=False
     )
-    if response.status_code == 404:
+    if response.status_code != 200:
         return None
 
-    results = response.json().get("data")
-    after = results.get("after")
-    count = results.get("dist")
+    try:
+        results = response.json().get("data", {})
+        children = results.get("children", [])
+        after = results.get("after")
 
-    for child in results.get("children"):
-        hot_list.append(child.get("data").get("title"))
+    except ValueError:
+        return None
+
+    for child in children:
+        hot_list.append(child.get("data", {}).get("title"))
         if after is not None:
-            return recurse(subreddit, hot_list, after, count)
+            return recurse(subreddit, hot_list, after)
         else:
             return hot_list
