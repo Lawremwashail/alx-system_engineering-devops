1b99c8625603bda9622d1af0f87f107719eb23f2
Lawremwashail <lawremwash@gmail.com>
2024-08-23 08:59:02 -0700

    sky-the-limit-NO

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,6 @@
-# fix nginx files limit for more requests
+# fix nginx files for more requests
 
-exec {'files limit':
-	command => 'sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart',
-	path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+exec {'nginx file limit and restart nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
-
