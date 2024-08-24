6734d6aa0f8b3178a1d165e5ad4999ab32eebe73
Lawremwashail <lawremwash@gmail.com>
2024-08-24 01:19:11 -0700

    user-limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 # Enables the user for 'holberton' to login and open files without error
 
 exec { 'increase-hard-file-limit-holberton-user':
-  command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf',
+  command => 'sed -i "/^holberton.*hard.*nofile/s/[0-9]\+/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
 exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
+  command => 'sed -i "/holberton.*soft.*nofile/s/[0-9]\+/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
